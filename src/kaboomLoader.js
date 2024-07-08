62cdedff60c242c084cd4889facbea417698e23b
parsTroy <troyparsons@hotmail.com>
2024-07-08 10:18:23 -0400

    Update scale

--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -2,8 +2,9 @@ import kaboom from "../lib/kaboom.mjs";
 
 export const scale = 2;
 export const k = kaboom({
-  width: 640,
-  height: 360,
+  width: 640 * scale,
+  height: 360 * scale,
+  scale,
   letterbox: true,
   global: false,
 });
